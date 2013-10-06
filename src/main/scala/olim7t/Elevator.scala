16d9432f7586b85aa0e07147166273ea792046cf
olim7t <omichallat+github@gmail.com>
2013-10-06 03:10:27 +0200

    Improve logging.

--- src/main/scala/olim7t/Elevator.scala
+++ src/main/scala/olim7t/Elevator.scala
@@ -7,15 +7,15 @@ import Protocol._
 class Elevator extends Actor with ActorLogging with ElevatorController {
   def receive = {
     case event: Event =>
-      log.debug(s"Handling ${event} - state before: ${dumpState}")
+      log.debug(s"[${event}] - state before: ${dumpState}")
       handle(event)
-      log.debug(s"Handling ${event} - state after:  ${dumpState}")
+      log.debug(s"   state after:  ${dumpState}")
       sender ! EventAck
 
     case NextCommand =>
-      log.debug(s"Returning command - state before: ${dumpState}")
+      log.debug(s"[nextCommand] - state before: ${dumpState}")
       val command = nextCommand
-      log.debug(s"Returning ${command} - state after: ${dumpState}")
+      log.debug(s"   returning ${command} - state after: ${dumpState}")
       sender ! command
   }
 }
